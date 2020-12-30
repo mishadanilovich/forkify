96cc241eeabcc23dcd7af0eb66832578636ee765
mishadanilovich <danilovich36misha@gmail.com>
2020-12-30 21:37:45 +0300

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'b11b3491-a9df-4e8d-8b93-9821f64dc749';
+export const MODAL_CLOSE_SEC = 2.5;
